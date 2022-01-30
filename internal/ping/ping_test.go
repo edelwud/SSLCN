caf5126fc7692f4a7956144df167ea50aef494ec
Maksim Yersh <edelwud@icloud.com>
2022-01-30 23:33:31 +0300

    add: multithreading test

--- internal/ping/ping_test.go
+++ internal/ping/ping_test.go
@@ -3,6 +3,7 @@ package ping
 import (
 	"github.com/davecgh/go-spew/spew"
 	"testing"
+	"time"
 )
 
 func TestNew(t *testing.T) {
@@ -31,4 +32,30 @@ func TestPing_Send(t *testing.T) {
 	}
 
 	spew.Dump(buf.Bytes())
+}
+
+func TestPing_Receive(t *testing.T) {
+	p, err := New("google.com")
+	if err != nil {
+		panic(err)
+	}
+	defer p.Close()
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			err := p.Send()
+			if err != nil {
+				panic(err)
+			}
+
+			value, err := p.Receive()
+			if err != nil {
+				panic(err)
+			}
+
+			spew.Dump(value.Bytes())
+		}()
+	}
+
+	time.Sleep(time.Second * 10)
 }
